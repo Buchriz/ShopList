5aab4ae7b0e637d5d5039d24197bb9498fc6acb6
Buchriz <s88100@nhs.co.il>
2024-01-09 14:07:19 +0200

    ShopList 2.0 + SQLite

--- app/src/main/java/com/example/shoplist/AddItemFragment.java
+++ app/src/main/java/com/example/shoplist/AddItemFragment.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class AddItemFragment extends Fragment implements View.OnClickListener {
+public class AddItemFragment extends Fragment {
 
     private  View rootView;
     private EditText etName;
     private String name;
     private Button btnAdd;
+
+    private MyDatabaseHelper databaseHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,41 +28,33 @@ public class AddItemFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_add_item, container, false);
-        initView();
+        databaseHelper = new MyDatabaseHelper(requireActivity());
+
+        etName = rootView.findViewById(R.id.etName);
+        btnAdd = rootView.findViewById(R.id.btnAddToList);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                name = etName.getText().toString().trim();
+                etName.setText("");
+
+                if (name.length() == 0)
+                {
+                    Toast.makeText(requireActivity(), "כתוב מוצר", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    ListFragment listFragment = new ListFragment();
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_fragment, listFragment).commit();
+                    databaseHelper.addProduct(name);
+                }
+            }
+        });
+
+
         return rootView;
     }
 
-    private void initView()
-    {
-        etName = rootView.findViewById(R.id.etName);
-
-        btnAdd = rootView.findViewById(R.id.btnAddToList);
-        btnAdd.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View view)
-    {
-        if (view == btnAdd)
-        {
-            name = etName.getText().toString();
-            etName.setText("");
-            
-            if (name.length() == 0)
-            {
-                Toast.makeText(requireActivity(), "כתוב מוצר", Toast.LENGTH_SHORT).show();
-            }
-            else 
-            {
-                Bundle bundle = new Bundle();
-
-                bundle.putString("name", name);
-                ListFragment listFragment = new ListFragment();
-                listFragment.setArguments(bundle);
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_fragment, listFragment).commit();
-            }
-        }
-    }
-    
 }
--- app/src/main/java/com/example/shoplist/ListFragment.java
+++ app/src/main/java/com/example/shoplist/ListFragment.java
@@ -1,6 +1,7 @@
 package com.example.shoplist;
 
 import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -26,8 +27,8 @@ public class ListFragment extends Fragment {
 
     private View rootView;
     private TableLayout tableLayout;
-    private String getName;
-    public static List<String> nameList = new LinkedList<>();
+    private static MyDatabaseHelper databaseHelper;
+    private Cursor cursor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,97 +36,106 @@ public class ListFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        rootView =  inflater.inflate(R.layout.fragment_list, container, false);
-        initView();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_list, container, false);
+
+        tableLayout = rootView.findViewById(R.id.tableLayout);
+        databaseHelper = new MyDatabaseHelper(requireActivity());
+        cursor = databaseHelper.readAllData();
+
+        int n = cursor.getCount();
+        cursor.moveToFirst();
+
+        for (int i = 0; i < n; i++) {
+            String name = cursor.getString(1);
+            int done = cursor.getInt(2);
+            addTableRow(name, done, Integer.parseInt(cursor.getString(0)));
+            cursor.moveToNext();
+        }
+
         return rootView;
     }
 
-    private void initView()
-    {
-        tableLayout = rootView.findViewById(R.id.tableLayout);
-
-        Bundle bundle = this.getArguments();
-        if (bundle != null)
-        {
-            getName = bundle.getString("name");
-            nameList.add(getName);
-        }
-        addTableRow();
-    }
 
     @SuppressLint("UseCompatLoadingForDrawables")
-    private void addTableRow()
+    private void addTableRow(String name, int done, int id)
     {
         tableLayout.setBackground(getResources().getDrawable(R.drawable.table_corners));
-        for (int i = 0; i < nameList.size(); i++)
-        {
-            TableRow row = new TableRow(requireActivity());
-            TableRow.LayoutParams rowp = new TableRow.LayoutParams(-2, -2);
-            row.setLayoutParams(rowp);
-            row.setBackground(getResources().getDrawable(R.drawable.table_corners));
-
-            TextView tvName = new TextView(requireActivity());
-            TableRow.LayoutParams ll = new TableRow.LayoutParams(-2, -2);
-            ll.setMargins(0,1,0,0);
-            tvName.setLayoutParams(ll);
-
-            if (nameList.get(i).length() > 10)
-            {
-                tvName.setText(DownRow(nameList.get(i)));
-            }
-            else
-            {
-                tvName.setText(nameList.get(i));
-            }
-            tvName.setTextColor(Color.BLACK);
-            tvName.setTextSize(26);
-            tvName.setGravity(Gravity.CENTER);
-            tvName.setBackground(getResources().getDrawable(R.drawable.table_corners));
-
-            CheckBox cb = new CheckBox(requireActivity());
-            TableRow.LayoutParams cbParams = new TableRow.LayoutParams(-2, 100);
-            cbParams.gravity = Gravity.CENTER;
-            cb.setLayoutParams(cbParams);
-
-            cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-                {
-                    if (isChecked)
-                    {
-                        tvName.setTextColor(ContextCompat.getColor(requireActivity(), R.color.blue));
-                    }
-                    else
-                    {
-                        tvName.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black));
-                    }
-                }
-            });
 
 
-            row.addView(cb);
-            row.addView(tvName);
-            tableLayout.addView(row);
+        TableRow row = new TableRow(requireActivity());
+        TableRow.LayoutParams rowp = new TableRow.LayoutParams(-2, -2);
+        row.setLayoutParams(rowp);
+        row.setBackground(getResources().getDrawable(R.drawable.table_corners));
+
+        TextView tvName = new TextView(requireActivity());
+        TableRow.LayoutParams ll = new TableRow.LayoutParams(-2, -2);
+        ll.setMargins(0, 1, 0, 0);
+        tvName.setLayoutParams(ll);
+
+        if (name.length() > 10) {
+            tvName.setText(DownRow(name).trim());
+        } else {
+            tvName.setText(name.trim());
         }
+        tvName.setTextSize(26);
+        tvName.setGravity(Gravity.CENTER);
+        if (done == 1)
+        {
+            tvName.setTextColor(Color.BLUE);
+        }
+        else {
+            tvName.setTextColor(Color.BLACK);
+        }
+        tvName.setBackground(getResources().getDrawable(R.drawable.table_corners));
+
+        CheckBox cb = new CheckBox(requireActivity());
+        TableRow.LayoutParams cbParams = new TableRow.LayoutParams(-2, 100);
+        cbParams.gravity = Gravity.CENTER;
+        cb.setTextColor(Color.BLACK);
+        cb.setLayoutParams(cbParams);
+        cb.setChecked(done == 1);
+
+
+
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked)
+                {
+                    tvName.setTextColor(Color.BLUE);
+                    databaseHelper.updateCheckBox(1, String.valueOf(id));
+                }
+                else
+                {
+                    tvName.setTextColor(Color.BLACK);
+                    databaseHelper.updateCheckBox(0, String.valueOf(id));
+                }
+            }
+        });
+
+
+        row.addView(cb);
+        row.addView(tvName);
+        tableLayout.addView(row);
+
+
     }
 
-    public String DownRow(String name)
-    {
+    public String DownRow(String name) {
         int index = 0;
         String str = "";
 
-        for (int i = 0; i < name.length(); i++)
-        {
+        for (int i = 0; i < name.length(); i++) {
             index++;
             str += name.charAt(i);
-            if (index >= 10)
-            {
+            if (index >= 10) {
                 str += "\n";
                 index = 0;
             }
         }
         return str;
     }
+
 }
